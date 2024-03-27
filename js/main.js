42ba6267f1a7a220ad36278de70e55e5ffbdccc7
jasonhowelldev <jasonhowelldev@proton.me>
2024-03-26 22:54:46 -0400

    npm express setup and serving of index.html

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
+const express = require('express')
+const app = express()
+const path = require('path')
 
+//Static Folder
+app.get('/', (req,res) => {
+    res.sendFile(path.resolve(__dirname + '/../public/index.html'))
+});
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
